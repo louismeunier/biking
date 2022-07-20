7a7f0cacce617ebc0005588f161081065869fefc
Louis Meunier <louismeunier123@gmail.com>
2022-07-20 13:35:08 -0400

    Make sync function check if ids already exist

--- functions/activity-sync/activity-sync.js
+++ functions/activity-sync/activity-sync.js
@@ -1,29 +1,49 @@
-import { postActivity, authorize } from "../lib/firebase-util"
+import { postActivity, authorize, getActivities } from "../lib/firebase-util"
 import { listActivityIds, getActivity } from "../lib/strava-api"
 
 const handler = async (event) => {
   try {
     const admin = await authorize();
     const db = admin.firestore();
-    const activityIds = await listActivityIds(db);
-    console.log(activityIds);
-    const activities = await Promise.all(activityIds.map(async (id) => {
-      const activity = await getActivity(id, db);
-      return activity;
-    }));
 
-    console.log(activities);
-    await Promise.all(activities.map(async (activity) => {
-      await postActivity(activity, db);
-    }));
-    await db.terminate();
-    await admin.app().delete();
-    return {
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
-      statusCode: 200,
-      body: JSON.stringify(activities)
+    const activityIds = await listActivityIds(db);
+
+    const dbActivities = await getActivities(db);
+    const dbActivitiesIds = dbActivities.map(activity => activity.id);
+
+    const missingIds = activityIds.filter(id => !dbActivitiesIds.includes(id));
+
+    if (missingIds.length == 0) {
+      await db.terminate();
+      await admin.app().delete();
+      
+      return {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        },
+        statusCode: 200,
+        body: JSON.stringify({"message": "Activities already synced"})
+      }
+    } else {
+      const activities = await Promise.all(activityIds.map(async (id) => {
+        const activity = await getActivity(id, db);
+        return activity;
+      }));
+
+      await Promise.all(activities.map(async (activity) => {
+        await postActivity(activity, db);
+      }));
+
+      await db.terminate();
+      await admin.app().delete();
+
+      return {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        },
+        statusCode: 200,
+        body: JSON.stringify({"message": "Activities synced"})
+      }
     }
   } catch (error) {
     console.log(error)
