8acb1476c1efb7ad8787efa8c8964e7bd9a29a7e
Louis Meunier <louismeunier123@gmail.com>
2022-07-22 09:47:02 -0400

    Import functions correctly

--- functions/webhook/webhook.js
+++ functions/webhook/webhook.js
@@ -1,4 +1,4 @@
-import { postActivity, deleteActivity } from "../lib/firebase-util";
+import { postActivity, deleteActivity, authorize } from "../lib/firebase-util";
 import { getActivity } from "../lib/strava-api";
 
 const handler = async (event, context) => {
@@ -11,70 +11,71 @@ const handler = async (event, context) => {
   const aspect_type = JSON.parse(event.body).aspect_type;
 
   try {
-  if (mode && token) {
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {     
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
+    if (mode && token) {
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {     
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        return {
+          statusCode: 200,
+          body: JSON.stringify({"hub.challenge":challenge})
+        }
+      } else {
+        return {
+          statusCode: 403,
+          body: JSON.stringify({'error':'Verification tokens do not match'})
+        }
+      }
+    } else if (aspect_type == "create") {
+      console.log("New activity created");
+      const activityId = event.body.object_id;
+      const admin = await authorize();
+      const db = admin.firestore();
+
+      const activityData = await getActivity(activityId, db);
+
+      await postActivity(activityData, db);
+
+      console.log(`Activity ${activityId} added to database`);
+
+      await db.terminate();
+      await admin.app().delete();
+
       return {
         statusCode: 200,
-        body: JSON.stringify({"hub.challenge":challenge})
+        body: JSON.stringify({"message": "Activity synced"})
+      }
+    } else if (event.body.aspect_type == "delete") {
+      console.log("Activity deleted");
+
+      const activityId = event.body.object_id;
+      const admin = await authorize();
+      const db = admin.firestore();
+
+      await deleteActivity(activityId, db);
+
+      console.log(`Activity ${activityId} deleted from database`);
+
+      await db.terminate();
+      await admin.app().delete();
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({"message": "Activity deleted"})
       }
     } else {
-      return {
-        statusCode: 403,
-        body: JSON.stringify({'error':'Verification tokens do not match'})
-      }
-    }
-  } else if (aspect_type == "create") {
-    console.log("New activity created");
-    const activityId = event.body.object_id;
-    const admin = await authorize();
-    const db = admin.firestore();
-
-    const activityData = await getActivity(activityId, db);
-
-    await postActivity(activityData, db);
-
-    console.log(`Activity ${activityId} added to database`);
-
-    await db.terminate();
-    await admin.app().delete();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({"message": "Activity synced"})
-    }
-  } else if (event.body.aspect_type == "delete") {
-    console.log("Activity deleted");
-
-    const activityId = event.body.object_id;
-    const admin = await authorize();
-    const db = admin.firestore();
-
-    await deleteActivity(activityId, db);
-
-    console.log(`Activity ${activityId} deleted from database`);
-
-    await db.terminate();
-    await admin.app().delete();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({"message": "Activity deleted"})
-    }
-  } else {
     console.log("Something went wrong");
     return {
       statusCode: 400,
       body: JSON.stringify({'error':'Missing mode or token'})
     }
-  } } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({'error':error})
+    } 
+  } catch (error) {
+      console.log(error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({'error':error})
+      }
     }
-  }
 }
 
 module.exports = { handler }
