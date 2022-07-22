817ac26f8f39d62ccfba0c8f319b9ef5006914b3
Louis Meunier <louismeunier123@gmail.com>
2022-07-22 09:03:03 -0400

    Make webhook function use javascript

---
+++ functions/lib/webhook.js
@@ -0,0 +1,64 @@
+const { postActivity } = require("../lib/firebase-util");
+const { getActivity } = require("../lib/strava-api");
+
+const handler = async (event, context) => {
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+  let mode = event.queryStringParameters['hub.mode'];
+  let token = event.queryStringParameters['hub.verify_token'];
+  let challenge = event.queryStringParameters['hub.challenge'];
+
+  if (mode && token) {
+
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {     
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      return {
+        statusCode: 200,
+        body: JSON.stringify({"hub.challenge":challenge})
+      }
+    } else {
+      return {
+        statusCode: 403,
+        body: JSON.stringify({'error':'Verification tokens do not match'})
+      }
+    }
+  } else if (event.body.aspect_type == "create") {
+    const activityId = event.body.object_id;
+    const admin = await authorize();
+    const db = admin.firestore();
+
+    const activityData = await getActivity(activityId, db);
+
+    await postActivity(activityData, db);
+
+    await db.terminate();
+    await admin.app().delete();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({"message": "Activity synced"})
+    }
+  } else if (event.body.aspect_type == "delete") {
+    const activityId = event.body.object_id;
+    const admin = await authorize();
+    const db = admin.firestore();
+
+    await deleteActivity(activityId, db);
+
+    await db.terminate();
+    await admin.app().delete();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({"message": "Activity deleted"})
+    }
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({'error':'Missing mode or token'})
+    }
+  }
+}
+
+module.exports = { handler }
