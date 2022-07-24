6a206249fa797709c382df192c8af8d300e36e6f
Louis Meunier <louismeunier123@gmail.com>
2022-07-24 12:16:23 -0400

    Handle errors in getActivites route

--- functions/get-activity/get-activity.js
+++ functions/get-activity/get-activity.js
@@ -2,18 +2,25 @@ import { getActivities, authorize } from '../lib/firebase-util';
 
 // get all activities from the database
 const handler = async function () {
-  const admin = await authorize();
-  const db = admin.firestore();
-  const data = await getActivities(db);
+  try {
+    const admin = await authorize();
+    const db = admin.firestore();
+    const data = await getActivities(db);
 
-  await db.terminate();
-  await admin.app().delete();
-  return {
-    headers: {
+    await db.terminate();
+    await admin.app().delete();
+    return {
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+  } catch (error) {
+    console.log(error)
+    return { headers: {
       "Access-Control-Allow-Origin": "*"
-    },
-    statusCode: 200,
-    body: JSON.stringify(data)
+    }, statusCode: 500, body: error.toString() }
   }
 }
 
