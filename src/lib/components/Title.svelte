54133cea248d44d699117f6fb6d1a10197d8e466
Louis Meunier <louismeunier123@gmail.com>
2022-07-25 21:10:51 -0400

    Add down arrow link... down

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -2,6 +2,9 @@
     <h1>Bike Trail Visualizer</h1>
     <img src="/bike.svg" alt="Bike Logo" height="55px"/>
     <p>An automatically updating map of my (Louis Meunier's) biking activity from Strava.</p>
+    <a href="#analysis" title="Go to analysis">
+        <img class="down" src="/down-arrow.png" alt="Down Arrow" height="25px"/>
+    </a>
 </div>
 
 <style>
@@ -17,4 +20,22 @@
         flex-direction: column;
         align-items: center;
     }
+
+    .down {
+        animation: bounce 2s infinite;
+    }
+
+    /* make an animation for the down arrow to bounce up and down, slowing at its peaks */
+    @keyframes bounce {
+        0% {
+            transform: translateY(0.75);
+        }
+        50% {
+            transform: translateY(-0.75em);
+        }
+        100% {
+            transform: translateY(0.75);
+        }
+    }
+
 </style>
