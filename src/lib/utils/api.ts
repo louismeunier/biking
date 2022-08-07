b8b423ed35d048a7546b5ce33138d7f381951ae7
Louis Meunier <louismeunier123@gmail.com>
2022-08-06 21:09:17 -0400

    Load from cache

--- src/lib/utils/api.ts
+++ src/lib/utils/api.ts
@@ -25,15 +25,44 @@ export interface Activity {
     }
 }
 
+async function fetchCached(url: string) {
+  const cache = JSON.parse(localStorage.getItem(url));
+  if (cache && cache.expires > Date.now()) {
+    return cache.data;
+  }
+  const request = await fetch(url);
+  const data = await request.json();
+  localStorage.setItem(url, JSON.stringify({
+    expires: Date.now() + 1000 * 60 * 60 * 24,
+    data: data,
+  }));
+  return data;
+}
+
 export async function getActivities(): Promise<Activity[]> {
   try {
     toast.push("Loading activitiy data...", { theme: themes.wait, dismissable: false, duration: 10000 });
-    const request = await fetch("https://magical-fox-098a60.netlify.app/.netlify/functions/get-activity");
-    const activities = await request.json();
+    const activities = await fetchCached("https://magical-fox-098a60.netlify.app/.netlify/functions/get-activity");
     toast.pop();
     toast.push("Activities loaded!", { theme: themes.success });
     return activities.map(a => { a.meta = { show: true, highlight: false } ; return a;});
   } catch (error) {
+    toast.pop()
+    toast.push("Error loading activities!", { theme: themes.error });
+    console.error(error);
+  }
+}
+
+export async function getActivityStreams(id): Promise<Activity[]> {
+  try {
+    toast.push("Loading activity data...", { theme: themes.wait, dismissable: false, duration: 10000 });
+    const request = await fetch(`https://magical-fox-098a60.netlify.app/.netlify/functions/get-activity-streams?id=${id}&streams=heartrate,time`);
+    const activitiesStreams = await request.json();
+    toast.pop();
+    toast.push("Activities loaded!", { theme: themes.success });
+    return activitiesStreams;
+  }
+  catch (error) {
     toast.pop()
     toast.push("Error loading activities!", { theme: themes.error });
     console.error(error);
