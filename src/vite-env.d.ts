32700f323b5eab62541d131619a84ac60077a960
Louis Meunier <louismeunier123@gmail.com>
2023-02-08 09:45:03 -0500

    Move custom types globally accessible

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,36 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+interface ActivityStream {
+    time: number[];
+    distance: number[];
+    // latlng: number[][],
+    altitude: number[];
+    heartrate: number[];
+    watts: number[];
+    temp: number[];
+}
+
+interface Activity {
+    id: number;
+    name: string;
+    type: string;
+    distance: number;
+    moving_time: number;
+    elapsed_time: number;
+    start_date: string;
+    map: string;
+    start_latlng: number[];
+    end_latlng: number[];
+    average_speed: number;
+    max_speed: number;
+    average_watts?: number;
+    kilojoules?: number;
+    average_heartrate?: number;
+    max_heartrate?: number;
+    calories?: number;
+    meta: {
+      show: boolean;
+      highlight: boolean;
+    }
+}
